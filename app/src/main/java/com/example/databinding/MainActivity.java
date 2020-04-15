cd16ed7c42bfbac61f7ec74dc2669d51a49e0534
mrtarunsaraswat@gmail.com <sharm@18091996>
2020-04-15 18:01:26 +0530

    With Click Handles

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.databinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.databinding.databinding.ActivityMainBinding;
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding activityMainBinding;
+    private MainActivityClickHandlers clickHandlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         activityMainBinding.setStudent(getCurrentStudent());
+
+        clickHandlers = new MainActivityClickHandlers(this);
+        activityMainBinding.setClickHandler(clickHandlers);
     }
 
     private Student getCurrentStudent(){
@@ -29,5 +36,22 @@ public class MainActivity extends AppCompatActivity {
         student.setStudentEmail("mrtarunsaraswat@gmail.com");
         return student;
 
+    }
+
+    public class MainActivityClickHandlers{
+
+        Context context;
+
+        public MainActivityClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        public void onEnrollButtonClicked(View view) {
+            Toast.makeText(context,"Enroll Button Clicked",Toast.LENGTH_SHORT).show();
+        }
+
+        public void onCancelButtonClicked(View view) {
+            Toast.makeText(context,"Cancel Button Clicked",Toast.LENGTH_SHORT).show();
+        }
     }
 }
